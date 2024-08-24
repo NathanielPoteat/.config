379d6ffe20d5f13b5046943b36690ffe4fa3ffc2
Nathaniel C. Poteat <ntn.poteat@gmail.com>
2024-08-24 15:30:19 -0400

    Wezterm identifies OS, sets default prompt and launch menu for Windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,45 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local opacity = 0.75
 local transparent_bg = "rgba(22, 24, 26, " .. opacity .. ")"
+local launch_menu = {}
+
+-- Determine OS
+local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
+local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
+local is_windows <const> = wezterm.target_triple:find("windows") ~= nil
+
+-- Default Shell and Launch Menu
+if is_windows then
+    config.default_prog = { 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe', '-NoLogo' }
+
+    table.insert(launch_menu, {
+        label = 'PowerShell',
+        args = { 'powershell.exe', '-NoLogo' },
+    })
+
+  -- Find installed visual studio version(s) and add their compilation environment command prompts to the menu
+  for _, vsvers in
+    ipairs(
+      wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
+    )
+  do
+    local year = vsvers:gsub('Microsoft Visual Studio/', '')
+    table.insert(launch_menu, {
+      label = 'x64 Native Tools VS ' .. year,
+      args = {
+        'cmd.exe',
+        '/k',
+        'C:/Program Files (x86)/'
+          .. vsvers
+          .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
+      },
+    })
+  end
+
+  config.launch_menu = launch_menu
+end
+--if is_linux or is_darwin then
+--    config.default_prog = -- zsh
 
 -- Font
 config.font = wezterm.font_with_fallback({
